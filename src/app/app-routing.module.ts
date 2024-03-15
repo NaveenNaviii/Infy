acb3e10e84c2f848b80caf86158424c0112e3b6f
NaveenNaviii <navee9900@gmail.com>
2024-03-15 17:31:30 +0530

    adde branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,12 @@ import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
   {path:'home',component:HomeComponent},
   {path:'regn',component:RegisterComponent},
   {path:'login',component:LoginComponent},
+  {path:'dashboard',loadChildren:()=>import('./product-dashboard/product-dashboard.module')
+    .then(x=>x.ProductDashboardModule)},
   {path:'**',component:PageNotFoundComponent}
 ];
 
